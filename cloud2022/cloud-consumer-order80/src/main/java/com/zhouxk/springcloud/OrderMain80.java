0f89153f57a2128fd42b73472018096170ea85da
zhouxk <843487518@qq.com>
2022-09-27 21:03:38 +0800

    使用Ribbon的负载均衡策略

--- cloud2022/cloud-consumer-order80/src/main/java/com/zhouxk/springcloud/OrderMain80.java
+++ cloud2022/cloud-consumer-order80/src/main/java/com/zhouxk/springcloud/OrderMain80.java
@@ -1,9 +1,11 @@
 package com.zhouxk.springcloud;
 
+import com.zhouxk.myrule.MyselfRule;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 /**
  * @BelongsProject: cloud2022
@@ -15,6 +17,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @Slf4j
 @EnableEurekaClient
+@RibbonClient(name = "CLOUD-PAYMENT-SERVICE", configuration = MyselfRule.class) // 启动该服务时去加载自定义的ribbon配置
 @SpringBootApplication
 public class OrderMain80 {
     public static void main(String[] args) {
